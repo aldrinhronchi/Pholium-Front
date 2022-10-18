c3d6a8d46df2f5cdcb68e50090d2d6ed3f9637b4
Terore <aldrinhronchi@gmail.com>
2022-10-17 22:53:47 -0300

    First Page
    Created Nav Bar, and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import {RouterModule} from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterBarComponent } from './footer-bar/footer-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    ProjectsComponent,
+    HomeComponent,
+    FooterBarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+      RouterModule.forRoot([
+        { path: '', component: HomeComponent, pathMatch: 'full' },
+        { path: 'projects', component: ProjectsComponent },
+      ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
